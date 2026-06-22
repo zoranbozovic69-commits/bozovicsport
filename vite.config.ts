066a078c7fcf899df43e0b1ff1f4ece3788bff46
zoranbozovic69-commits <zoranbozovic69@gmail.com>
2026-06-22 14:46:20 +0200

    Added base route for vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/bozovicsport/',
   server: {
     host: "::",
     port: 8080,
