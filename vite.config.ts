16142ee6f80f47f8e2fac2b0cfabb7f273e5ef33
zoranbozovic69-commits <zoranbozovic69@gmail.com>
2026-06-22 16:22:25 +0200

    Removed base route from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/bozovicsport/',
   server: {
     host: "::",
     port: 8080,
